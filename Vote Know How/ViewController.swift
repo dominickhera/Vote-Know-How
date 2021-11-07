9b0efb5787bb69cfd8f26057ace0b1652f882b2e
dominick hera <dominickhera@me.com>
2021-11-06 20:11:42 -0700

    Initial Commit

---
+++ Vote Know How/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vote Know How
+//
+//  Created by Dominick Hera on 11/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
