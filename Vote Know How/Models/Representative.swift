eff18b97b1a5f9e530d5baac37dedeb479e32071
dominick hera <dominickhera@me.com>
2021-11-23 14:42:37 -0500

    representative decoding

--- Vote Know How/Models/Representative.swift
+++ Vote Know How/Models/Representative.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Representative
+class Representative: NSObject, Decodable
 {
     var id: String
     var title: String
@@ -39,7 +39,6 @@ class Representative
     var dwNominate: Float?
     var idealPoint: Float?
     var seniority: Int
-    var nextElection: String?
     var totalVotes: Int
     var missedVotes: Int
     var totalPresent: Int
@@ -49,11 +48,101 @@ class Representative
     var phone: String?
     var fax: String?
     var state: String
-    var senateClass: String
-    var stateRank: String
-    var lisID: String
     var missedVotesPercent: Float?
     var votesWithPartyPercent: Float?
     var votesAgainstPartyPercent: Float?
     
+    enum CodingKeys: String, CodingKey
+    {
+        case id
+        case title
+        case shortTitle = "short_title"
+        case api_uri
+        case firstName = "first_name"
+        case middleName = "middle_name"
+        case lastName = "last_name"
+        case suffix
+        case dateOfBirth = "date_of_birth"
+        case gender
+        case party
+        case leadershipRole = "leadership_role"
+        case twitterAccount = "twitter_account"
+        case facebookAccount = "facebook_account"
+        case youtubeAccount = "youtube_account"
+        case govTrackID = "govtrack_id"
+        case cspanID = "cspan_id"
+        case voteSmartID = "votesmart_id"
+        case icpsrID = "icpsr_id"
+        case crpID = "crp_id"
+        case googleEntityID = "google_entity_id"
+        case fecCandidateID = "fec_candidate_id"
+        case url
+        case rssURL = "rss_url"
+        case contactForm = "contact_form"
+        case inOffice = "in_office"
+        case cookPvi = "cook_pvi"
+        case dwNominate = "dw_nominate"
+        case idealPoint = "ideal_point"
+        case seniority
+        case totalVotes = "total_votes"
+        case missedVotes = "missed_votes"
+        case totalPresent = "total_present"
+        case lastUpdated = "last_updated"
+        case ocdID = "ocd_id"
+        case office
+        case phone
+        case fax
+        case state
+        case missedVotesPercent = "missed_votes_pct"
+        case votesWithPartyPercent = "votes_with_party_pct"
+        case votesAgainstPartyPercent = "votes_against_party_pct"
+    }
+        
+    required init(from decoder: Decoder) throws
+    {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(String.self, forKey: .id)
+        title = try values.decode(String.self, forKey: .title)
+        shortTitle = try values.decode(String.self, forKey: .shortTitle)
+        api_uri = try values.decodeIfPresent(URL.self, forKey: .api_uri)
+        firstName = try values.decode(String.self, forKey: .firstName)
+        middleName = try values.decodeIfPresent(String.self, forKey: .middleName)
+        lastName = try values.decode(String.self, forKey: .lastName)
+        suffix = try values.decodeIfPresent(String.self, forKey: .suffix)
+        gender = try values.decode(String.self, forKey: .gender)
+        party = try values.decode(String.self, forKey: .party)
+        leadershipRole = try values.decodeIfPresent(String.self, forKey: .leadershipRole)
+        twitterAccount = try values.decodeIfPresent(String.self, forKey: .twitterAccount)
+        facebookAccount = try values.decodeIfPresent(String.self, forKey: .facebookAccount)
+        youtubeAccount = try values.decodeIfPresent(String.self, forKey: .youtubeAccount)
+        govTrackID = try values.decodeIfPresent(String.self, forKey: .govTrackID)
+        cspanID = try values.decodeIfPresent(String.self, forKey: .cspanID)
+        voteSmartID = try values.decodeIfPresent(String.self, forKey: .voteSmartID)
+        icpsrID = try values.decodeIfPresent(String.self, forKey: .icpsrID)
+        crpID = try values.decodeIfPresent(String.self, forKey: .crpID)
+        googleEntityID = try values.decodeIfPresent(String.self, forKey: .googleEntityID)
+        fecCandidateID = try values.decodeIfPresent(String.self, forKey: .fecCandidateID)
+        url = try values.decodeIfPresent(URL.self, forKey: .url)
+        rssURL = try values.decodeIfPresent(URL.self, forKey: .rssURL)
+        contactForm = try values.decodeIfPresent(URL.self, forKey: .contactForm)
+        inOffice = try values.decode(Bool.self, forKey: .inOffice)
+        cookPvi = try values.decodeIfPresent(String.self, forKey: .cookPvi)
+        dwNominate = try values.decodeIfPresent(Float.self, forKey: .dwNominate)
+        idealPoint = try values.decodeIfPresent(Float.self, forKey: .idealPoint)
+        seniority = try values.decode(Int.self, forKey: .seniority)
+        totalVotes = try values.decode(Int.self, forKey: .totalVotes)
+        missedVotes = try values.decode(Int.self, forKey: .missedVotes)
+        totalPresent = try values.decode(Int.self, forKey: .totalPresent)
+        ocdID = try values.decodeIfPresent(String.self, forKey: .ocdID)
+        office = try values.decodeIfPresent(String.self, forKey: .office)
+        phone = try values.decodeIfPresent(String.self, forKey: .phone)
+        fax = try values.decodeIfPresent(String.self, forKey: .fax)
+        state = try values.decode(String.self, forKey: .state)
+        missedVotesPercent = try values.decodeIfPresent(Float.self, forKey: .missedVotesPercent)
+        votesWithPartyPercent = try values.decodeIfPresent(Float.self, forKey: .votesWithPartyPercent)
+        votesAgainstPartyPercent = try values.decodeIfPresent(Float.self, forKey: .votesAgainstPartyPercent)
+        }
+
+//    var dateOfBirth: Date?
+//    var lastUpdated: Date?
 }
